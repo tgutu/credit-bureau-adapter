f7f4400da4bfe70603e9a84847029504065a02ae
Tapiwa Gutu <tapiwa.gutu@gmail.com>
2025-10-07 12:28:04 +0200

    * Wired up the main function to start the HTTP and gRPC serving the CreditBureauAdapterService

---
+++ cmd/cba/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"flag"
+	"net/http"
+
+	"github.com/tgutu/credit-bureau-adapter/internal/cba"
+	"github.com/tgutu/credit-bureau-adapter/internal/config"
+	"github.com/tgutu/credit-bureau-adapter/internal/server"
+	"go.uber.org/fx"
+	"go.uber.org/fx/fxevent"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	configFilename := flag.String("port", "cba.yaml", "Path to the configuration file")
+	flag.Parse()
+
+	configFile, err := config.NewConfig(*configFilename)
+	if err != nil {
+		panic(err)
+	}
+
+	fx.New(
+		fx.Invoke(
+			func(*grpc.Server) {},
+			func(*http.Server) {},
+		),
+		fx.Provide(func() *config.Config {
+			return configFile
+		}),
+		fx.Provide(
+			server.NewGrpcServer,
+			server.NewHTTPServer,
+			cba.NewServer,
+			zap.NewProduction,
+		),
+		fx.WithLogger(func(log *zap.Logger) fxevent.Logger {
+			return &fxevent.ZapLogger{Logger: log}
+		}),
+	).Run()
+}
