9be96c13bd30c7900304940280bb91f6259e0970
Tapiwa Gutu <tapiwa.gutu@gmail.com>
2025-10-07 12:14:03 +0200

    * Implement the abstract CreditBureauServiceAdapter which returns 'HTTP 501 Implemeneted' responses

---
+++ internal/cba/server.go
@@ -0,0 +1,23 @@
+package cba
+
+import (
+	"github.com/tgutu/credit-bureau-adapter/pkg/pb/cba/v1"
+	"go.uber.org/fx"
+	"go.uber.org/zap"
+)
+
+type ServiceParams struct {
+	fx.In
+	logger *zap.Logger
+}
+
+type server struct {
+	cba.UnimplementedCreditBureauAdapterServiceServer
+	logger *zap.Logger
+}
+
+func NewServer(lc fx.Lifecycle, params ServiceParams) cba.CreditBureauAdapterServiceServer {
+	return &server{
+		logger: params.logger,
+	}
+}
